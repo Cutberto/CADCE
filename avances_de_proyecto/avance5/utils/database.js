b84b9a955c0001dca229a70579a2560ca4f3df2d
cutberto <cutbertoan@gmail.com>
2021-04-09 16:08:07 -0500

    reajuste de views login y casos de uso

--- avances_de_proyecto/avance5/utils/database.js
+++ avances_de_proyecto/avance5/utils/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'cadce_test',
+    database: 'cadce_test_new',
     password: ''
 });
 
