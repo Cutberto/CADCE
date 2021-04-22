d69fad1a0ccba78b84681420c6ad7e3209d3cf9c
cutberto <cutbertoan@gmail.com>
2021-04-22 15:07:34 -0500

    deshacer error de carpetas

--- utils/is-admin.js
+++
@@ -1,6 +0,0 @@
-module.exports = (request, response, next) => {
-    if (request.session.rol != 1) { //rol 1 = admin,   rol 2 = usuario normal
-        return response.redirect('/proyectos/todos');
-    }
-    next();
-}
