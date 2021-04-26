1e38ada4792e0e57a0321af4fb4bf7d6280c2f84
Diego <A01552594@itesm.mx>
2021-04-26 11:19:38 -0500

    botones

--- avances_de_proyecto/avance5/utils/database.js
+++ avances_de_proyecto/avance5/utils/database.js
@@ -1,10 +1,14 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'cadce_test',
-    password: ''
+//    host: 'localhost',
+//    user: 'root',
+//    database: 'cadce_test',
+//    password: ''
+    host: 'bb8c6tkoblg2qoca2wqb-mysql.services.clever-cloud.com',
+    user: 'uwo0i7fedy6xkesb',
+    database: 'bb8c6tkoblg2qoca2wqb',
+    password: 'EofUZmotE7KtZ5eHhLy4'    
 });
 
 module.exports = pool.promise();
